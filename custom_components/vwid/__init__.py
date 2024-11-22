5730b01da672c02881595036a5fc4c115946970d
Jon Petter Skagmo <jon.petter@skagmo.com>
2024-11-22 09:18:06 +0100

    Remove use of deprecated aliases etc.

--- custom_components/vwid/__init__.py
+++ custom_components/vwid/__init__.py
@@ -2,11 +2,6 @@ from .const import DOMAIN
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant import config_entries, core
 
-async def async_setup(hass, config):
-# 	hass.async_add_job(async_load_platform(hass, 'sensor', DOMAIN, {}, config))
-	return True
-	
-	
 async def async_setup_entry(
     hass: core.HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
@@ -15,8 +10,6 @@ async def async_setup_entry(
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
